66569b47b462162654c44ba796709d553fe331fb
Vadim Chorrny <chorrny228@gmail.com>
2022-01-07 20:15:32 +0200

    add base models

---
+++ SushiSite/Models/Food.cs
@@ -0,0 +1,14 @@
+﻿namespace SushiSite.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public double Weight { get; set; }
+        public int Amount { get; set; }
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ SushiSite/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SushiSite.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public int Price { get; set; }
+        public IEnumerable<Food> Foods { get; set; }
+
+    }
+}
