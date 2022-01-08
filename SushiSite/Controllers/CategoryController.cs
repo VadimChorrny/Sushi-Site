ed886e636596b2ce5be5cf79d8dd65f96a3f0e8c
Vadim Chorrny <chorrny228@gmail.com>
2022-01-08 09:38:31 +0200

    - add contacts page and connect google maps

---
+++ SushiSite/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SushiSite.Data;
+using System.Linq;
+
+namespace SushiSite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Categories.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
--- SushiSite/Controllers/HomeController.cs
+++ SushiSite/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace SushiSite.Controllers
         {
             return View();
         }
+        public IActionResult Contacts()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
