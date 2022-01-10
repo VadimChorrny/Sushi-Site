db6782eb0c9dd5e43e82510ce9c2b5e671e3a80a
Vadim Chorrny <chorrny228@gmail.com>
2022-01-10 08:57:09 +0200

    add crud with category

--- SushiSite/Controllers/CategoryController.cs
+++ SushiSite/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SushiSite.Data;
+using SushiSite.Models;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SushiSite.Controllers
@@ -18,6 +21,52 @@ namespace SushiSite.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid) return View();
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            var categoryToRemove = _context.Categories.Find(id);
+            if (categoryToRemove == null) return NotFound();
+            _context.Categories.Remove(categoryToRemove);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id <= 0) return NotFound();
+
+            var ganre = _context.Categories.Find(id);
+
+            if (ganre == null) return NotFound();
+
+            IEnumerable<SelectListItem> categories = _context.Categories.Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+            ViewBag.CategoryList = categories;
+
+            return View(ganre);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (!ModelState.IsValid) return View();
+
+            _context.Categories.Update(obj);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
