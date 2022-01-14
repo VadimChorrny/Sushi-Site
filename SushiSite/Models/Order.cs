a8161e26a2aedd729a6fd6236386e11c38d72d27
Vadim Chorrny <chorrny228@gmail.com>
2022-01-14 07:51:58 +0200

    - fix bug with add orders

--- SushiSite/Models/Order.cs
+++ SushiSite/Models/Order.cs
@@ -1,8 +1,10 @@
-﻿namespace SushiSite.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SushiSite.Models
 {
     public class Order
     {
-        public int Id { get; set; }
+        public int OrderId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string LastName { get; set; }
